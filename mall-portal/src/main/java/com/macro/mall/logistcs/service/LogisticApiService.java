cf4569f066a2484f2abb56d225318a42e5271189
lilei <lileichenchen@sina.com>
2019-03-14 11:41:26 +0800

    init

--- mall-portal/src/main/java/com/macro/mall/logistcs/service/LogisticApiService.java
+++ mall-portal/src/main/java/com/macro/mall/logistcs/service/LogisticApiService.java
@@ -1,15 +1,14 @@
 package com.macro.mall.logistcs.service;
 
-import com.macro.mall.exception.LogisticsException;
-import com.macro.mall.logistcs.LogisticTrackModel;
 import com.macro.mall.logistcs.bean.ZhRecordBean;
+import com.macro.mall.logistcs.model.LogisticTrackModel;
 
 import java.util.List;
 
 public interface LogisticApiService {
+    List<LogisticTrackModel> getLogisticTrack(String expressNo);
+
     String recordLogistic(ZhRecordBean orderBean);
 
     void uploadIdentify(String name, String identifyNo, String frontUrl, String backUrl);
-
-    List<LogisticTrackModel> getLogisticTrack(String expressNo);
 }
--- mall-portal/src/main/java/com/macro/mall/logistcs/service/ZhLogisticApiServiceImpl.java
+++ mall-portal/src/main/java/com/macro/mall/logistcs/service/ZhLogisticApiServiceImpl.java
@@ -1,11 +1,11 @@
 package com.macro.mall.logistcs.service;
 
 import com.macro.mall.exception.LogisticsException;
-import com.macro.mall.logistcs.LogisticTrackModel;
 import com.macro.mall.logistcs.bean.ZhLogisTrackBackBean;
 import com.macro.mall.logistcs.bean.ZhLogisTrackBackListBean;
 import com.macro.mall.logistcs.bean.ZhRecordBackBean;
 import com.macro.mall.logistcs.bean.ZhRecordBean;
+import com.macro.mall.logistcs.model.LogisticTrackModel;
 import com.macro.mall.logistcs.util.ZhUtils;
 import org.apache.commons.collections4.CollectionUtils;
 import org.springframework.stereotype.Service;
@@ -15,6 +15,21 @@ import java.util.stream.Collectors;
 
 @Service
 public class ZhLogisticApiServiceImpl implements LogisticApiService {
+    @Override
+    public List<LogisticTrackModel> getLogisticTrack(String expressNo) {
+        ZhLogisTrackBackBean zhLogisTrackBackBean = null;
+        try {
+            zhLogisTrackBackBean = ZhUtils.logisticsTrack(expressNo);
+            List<ZhLogisTrackBackListBean> logisticsback = zhLogisTrackBackBean.getLogisticsback();
+            if (CollectionUtils.isNotEmpty(logisticsback)) {
+                return logisticsback.stream().map(logic -> new LogisticTrackModel(logic.getZtai(), logic.getTime(), "")).collect(Collectors.toList());
+            }
+        } catch (LogisticsException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
     @Override
     public String recordLogistic(ZhRecordBean zhRecordBean) {
         try {
@@ -31,20 +46,5 @@ public class ZhLogisticApiServiceImpl implements LogisticApiService {
     @Override
     public void uploadIdentify(String name, String identifyNo, String frontUrl, String backUrl) {
 
-    }
-
-    @Override
-    public List<LogisticTrackModel> getLogisticTrack(String expressNo) {
-        ZhLogisTrackBackBean zhLogisTrackBackBean = null;
-        try {
-            zhLogisTrackBackBean = ZhUtils.logisticsTrack(expressNo);
-            List<ZhLogisTrackBackListBean> logisticsback = zhLogisTrackBackBean.getLogisticsback();
-            if (CollectionUtils.isNotEmpty(logisticsback)) {
-                return logisticsback.stream().map(logic -> new LogisticTrackModel(logic.getZtai(), logic.getTime())).collect(Collectors.toList());
-            }
-        } catch (LogisticsException e) {
-            e.printStackTrace();
-        }
-        return null;
     }
 }
